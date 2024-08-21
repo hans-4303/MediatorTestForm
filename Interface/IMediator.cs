e11dca926bc3a710d87eef336e9cd21bf06464ba
hans-4303 <103019360+hans-4303@users.noreply.github.com>
2024-08-21 14:52:13 +0900

    인터페이스 주석 추가

--- Interface/IMediator.cs
+++ Interface/IMediator.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace MediatorTestForm.Interface
 {
+	/// <summary>
+	/// 중재자 인터페이스, 중재자 클래스는 인터페이스를 상속
+	/// </summary>
 	public interface IMediator
 	{
+		/// <summary>
+		/// 중재자 클래스가 상속하고 구현할 메서드
+		/// </summary>
+		/// <param name="sender">컴포넌트 등이 가능</param>
+		/// <param name="ev">이벤트 값</param>
 		void Notify (object sender, string ev);
 	}
 }
